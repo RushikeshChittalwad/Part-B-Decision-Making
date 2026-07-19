3833836a7cccee628ba74036ac95dbe638f8608c
RushikeshChittalwad <rushikeshchittalwad@gmail.com>
2026-07-19 10:20:08 +0530

    Add character type identification program

---
+++ alphabetdigitorsymbol.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+    if (isalpha(ch)) {
+        printf("'%c' is an alphabet.\n", ch);
+    } else if (isdigit(ch)) {
+        printf("'%c' is a digit.\n", ch);
+    } else {
+        printf("'%c' is a special symbol.\n", ch);
+    }
+
+    return 0;
+}
